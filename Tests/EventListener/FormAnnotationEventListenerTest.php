925563dbc1325178edb044bea981ade534dd13ce
mmoreram <marc.morera@befactory.es>
2013-12-17 19:16:17 +0100

    Added empty FormAnnotationEntityListener file

---
+++ Tests/EventListener/FormAnnotationEventListenerTest.php
@@ -0,0 +1,18 @@
+<?php
+
+/**
+ * Controller Extra Bundle
+ *
+ * @author Marc Morera <yuhu@mmoreram.com>
+ * @since 2013
+ */
+
+namespace Mmoreram\ControllerExtraBundle\Tests\EventListener;
+
+/**
+ * Tests FormAnnotationEventListener class
+ */
+class FormAnnotationEventListenerTest extends \PHPUnit_Framework_TestCase
+{
+
+}
