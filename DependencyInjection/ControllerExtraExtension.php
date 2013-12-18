eb4f301fc225b6fda92a5743d791ae619d819d80
mmoreram <marc.morera@befactory.es>
2013-12-18 19:30:53 +0100

    Deleted unused files and organized and documented bundle extension

--- DependencyInjection/ControllerExtraExtension.php
+++ DependencyInjection/ControllerExtraExtension.php
@@ -27,42 +27,85 @@ class ControllerExtraExtension extends Extension
         $configuration = new Configuration();
         $config = $this->processConfiguration($configuration, $configs);
 
+        /**
+         * Form parameters
+         */
         $container->setParameter('mmoreram.controllerextra.form_active', $config['form']['active']);
 
+        /**
+         * Flush parameters
+         */
         $container->setParameter('mmoreram.controllerextra.flush_active', $config['flush']['active']);
         $container->setParameter('mmoreram.controllerextra.flush_default_manager', $config['flush']['default_manager']);
 
+        /**
+         * Log parameters
+         */
         $container->setParameter('mmoreram.controllerextra.log_active', $config['log']['active']);
         $container->setParameter('mmoreram.controllerextra.log_default_handler', $config['log']['default_handler']);
 
+        /**
+         * Paginator parameters
+         */
         $container->setParameter('mmoreram.controllerextra.paginator_active', $config['paginator']['active']);
         $container->setParameter('mmoreram.controllerextra.paginator_number_default', $config['paginator']['number_default']);
         $container->setParameter('mmoreram.controllerextra.paginator_page_default', $config['paginator']['page_default']);
         $container->setParameter('mmoreram.controllerextra.paginator_orderby_field_default', $config['paginator']['orderby_field_default']);
         $container->setParameter('mmoreram.controllerextra.paginator_orderby_mode_default', $config['paginator']['orderby_mode_default']);
 
+        /**
+         * Load config files
+         */
         $loader = new YamlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
         $loader->load('parameters.yml');
         $loader->load('event_listeners.yml');
 
+        $this->loadResolverConfiguration($loader, $config);
+    }
+
+
+    /**
+     * Load resolver config files
+     *
+     * @param YamlFileLoader $loader Loader
+     * @param array          $config Config
+     *
+     * @return ControllerExtraExtension self Object
+     */
+    public function loadResolverConfiguration(YamlFileLoader $loader, array $config)
+    {
+        /**
+         * Only load resolver form config definition if is active
+         */
         if ($config['form']['active']) {
 
             $loader->load('resolver_form.yml');
         }
 
+        /**
+         * Only load resolver flush config definition if is active
+         */
         if ($config['flush']['active']) {
 
             $loader->load('resolver_flush.yml');
         }
 
+        /**
+         * Only load resolver log config definition if is active
+         */
         if ($config['log']['active']){
 
             $loader->load('resolver_log.yml');
         }
 
+        /**
+         * Only load resolver paginator config definition if is active
+         */
         if ($config['paginator']['active']) {
 
             $loader->load('resolver_paginator.yml');
         }
+
+        return $this;
     }
 }
